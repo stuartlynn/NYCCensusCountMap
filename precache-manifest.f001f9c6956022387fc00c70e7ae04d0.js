75af60d7c001f2f3b7e78bcc36b113141a1053d3
Stuart Lynn <stuart.lynn@gmail.com>
2020-01-20 14:25:37 -0500

    Updates

---
+++ precache-manifest.f001f9c6956022387fc00c70e7ae04d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09face97f2a9d54b193b222e7a4d2bbe",
+    "url": "stuartlynn.github.io/NYCCensusCountMap/index.html"
+  },
+  {
+    "revision": "6e5c7ae1b7d1da427b3c",
+    "url": "stuartlynn.github.io/NYCCensusCountMap/static/css/2.e8fdfe18.chunk.css"
+  },
+  {
+    "revision": "aeddbc63df1a2581603c",
+    "url": "stuartlynn.github.io/NYCCensusCountMap/static/css/main.1a021df8.chunk.css"
+  },
+  {
+    "revision": "6e5c7ae1b7d1da427b3c",
+    "url": "stuartlynn.github.io/NYCCensusCountMap/static/js/2.37ae33cb.chunk.js"
+  },
+  {
+    "revision": "85031fb547e284e474bee248938c7e6e",
+    "url": "stuartlynn.github.io/NYCCensusCountMap/static/js/2.37ae33cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aeddbc63df1a2581603c",
+    "url": "stuartlynn.github.io/NYCCensusCountMap/static/js/main.2ffbe54a.chunk.js"
+  },
+  {
+    "revision": "b0621a5e35c1fbce5fa7",
+    "url": "stuartlynn.github.io/NYCCensusCountMap/static/js/runtime-main.605e1a50.js"
+  }
+]);
