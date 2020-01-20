9f5a9eb4d4641a2edd8f7b214c2f8ab9e88614df
Stuart Lynn <stuart.lynn@gmail.com>
2020-01-20 14:24:10 -0500

    Code for deploying to GHPages

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Route path="/" component={MainPage} />
       </Router>
     </div>
