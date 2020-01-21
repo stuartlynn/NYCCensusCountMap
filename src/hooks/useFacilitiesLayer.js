360545dd9dceccaba69e6d7a22c2d3ff554eee0a
Stuart Lynn <stuart.lynn@gmail.com>
2020-01-20 23:47:16 -0500

    Fixing deployed marker bug

--- src/hooks/useFacilitiesLayer.js
+++ src/hooks/useFacilitiesLayer.js
@@ -8,28 +8,31 @@ export default function useFacilitiesLayer(map, visible) {
     console.log('Map ', map, ' Facilities ', facilities);
     if (map.current && facilities) {
       map.current.on('load', () => {
-        map.current.loadImage('/marker.png', (error, image) => {
-          if (error) throw error;
-          map.current.addImage('marker', image);
-          map.current.addLayer({
-            id: 'facilities',
-            type: 'symbol',
-            source: {
-              type: 'geojson',
-              data: facilities,
-            },
-            layout: {
-              'icon-image': 'marker',
-              'icon-size': 0.4,
-              'text-field': ['get', 'facname'],
-              'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
-              'text-offset': [0, 0.6],
-              'text-size': 10,
-              'text-anchor': 'top',
-              visibility: visible ? 'visible' : 'none',
-            },
-          });
-        });
+        map.current.loadImage(
+          `${process.env.PUBLIC_URL}/marker.png`,
+          (error, image) => {
+            if (error) throw error;
+            map.current.addImage('marker', image);
+            map.current.addLayer({
+              id: 'facilities',
+              type: 'symbol',
+              source: {
+                type: 'geojson',
+                data: facilities,
+              },
+              layout: {
+                'icon-image': 'marker',
+                'icon-size': 0.4,
+                'text-field': ['get', 'facname'],
+                'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+                'text-offset': [0, 0.6],
+                'text-size': 10,
+                'text-anchor': 'top',
+                visibility: visible ? 'visible' : 'none',
+              },
+            });
+          },
+        );
       });
     }
   }, [map, facilities]);
