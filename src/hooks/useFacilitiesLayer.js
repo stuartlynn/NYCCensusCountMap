f5bb2669c9295845c543d533d1d673e789940029
Stuart Lynn <stuart.lynn@gmail.com>
2020-02-12 11:51:12 -0500

    Adding icons

--- src/hooks/useFacilitiesLayer.js
+++ src/hooks/useFacilitiesLayer.js
@@ -1,44 +1,68 @@
 import React, {useEffect} from 'react';
 import useFacilities from './useFacilities';
 
+function loadAllImages(map) {
+  const images = [
+    'Dot_Hospitals.png',
+    'Dot_FBO.png',
+    'Dot_CommunitySchools.png',
+    'Dot_LGBTQ.png',
+    'Dot_FoodKitchens.png',
+    'Dot_SeniorCenters.png',
+    'Dot_UniPreK.png',
+    'Dot_K12.png',
+    'Dot_CommunityCenters.png',
+    'Dot_MentalHealth.png',
+    'Dot_CBO.png',
+    'Dot_Libraries.png',
+  ];
+  return Promise.all(
+    images.map(
+      img =>
+        new Promise((resolve, reject) => {
+          map.loadImage(`${process.env.PUBLIC_URL}/imgs/${img}`, function(
+            error,
+            res,
+          ) {
+            map.addImage(img, res);
+            resolve();
+          });
+        }),
+    ),
+  );
+}
+
 export default function useFacilitiesLayer(map, visible, layer, id, types) {
   const facilities = useFacilities();
 
   useEffect(() => {
-    console.log('Map ', map, ' Facilities ', facilities);
     if (map.current && facilities) {
-      console.log('Facilities are ', facilities);
       map.current.on('load', () => {
-        map.current.loadImage(
-          `${process.env.PUBLIC_URL}/marker.png`,
-          (error, image) => {
-            if (error) throw error;
-            map.current.addImage('marker', image);
-            map.current.addLayer({
-              id: 'facilities',
-              type: 'symbol',
-              source: {
-                type: 'geojson',
-                data: facilities,
-              },
-              layout: {
-                'icon-image': 'marker',
-                'icon-size': 0.2,
-                'text-field': ['get', 'name'],
-                'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
-                'text-offset': [0, 0.6],
-                'text-size': 10,
-                'text-anchor': 'top',
-                //'icon-allow-overlap': true,
-                //              'text-allow-overlap': true,
-                visibility: visible ? 'visible' : 'none',
-              },
-              paint: {
-                'text-color': 'rgba(255,255,255,1)',
-              },
-            });
-          },
-        );
+        loadAllImages(map.current).then(() => {
+          map.current.addLayer({
+            id: 'facilities',
+            type: 'symbol',
+            source: {
+              type: 'geojson',
+              data: facilities,
+            },
+            layout: {
+              'icon-image': ['get', 'icon'],
+              'icon-size': 0.02,
+              'text-field': ['get', 'name'],
+              'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+              'text-offset': [0, 0.6],
+              'text-size': 10,
+              'text-anchor': 'top',
+              //'icon-allow-overlap': true,
+              //              'text-allow-overlap': true,
+              visibility: visible ? 'visible' : 'none',
+            },
+            paint: {
+              'text-color': 'rgba(255,255,255,1)',
+            },
+          });
+        });
       });
     }
   }, [map, facilities]);
