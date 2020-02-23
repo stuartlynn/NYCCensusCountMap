c197bee6beacb780007c735d0f5d91326a7baa10
Stuart Lynn <stuart.lynn@gmail.com>
2020-02-23 16:33:23 -0500

    feature categories actually working on map + more robust metrics changing

--- src/hooks/useFacilitiesLayer.js
+++ src/hooks/useFacilitiesLayer.js
@@ -1,95 +1,112 @@
-import React, {useEffect} from 'react';
-import useFacilities from './useFacilities';
+import React, { useEffect } from "react";
+import useFacilities from "./useFacilities";
 
 function loadAllImages(map) {
-  const images = [
-    'Dot_Hospitals.png',
-    'Dot_FBO.png',
-    'Dot_CommunitySchools.png',
-    'Dot_LGBTQ.png',
-    'Dot_FoodKitchens.png',
-    'Dot_SeniorCenters.png',
-    'Dot_UniPreK.png',
-    'Dot_K12.png',
-    'Dot_CommunityCenters.png',
-    'Dot_MentalHealth.png',
-    'Dot_CBO.png',
-    'Dot_Libraries.png',
-  ];
-  return Promise.all(
-    images.map(
-      img =>
-        new Promise((resolve, reject) => {
-          map.loadImage(`${process.env.PUBLIC_URL}/imgs/${img}`, function(
-            error,
-            res,
-          ) {
-            map.addImage(img, res);
-            resolve();
-          });
-        }),
-    ),
-  );
+    const images = [
+        "Dot_Hospitals.png",
+        "Dot_FBO.png",
+        "Dot_CommunitySchools.png",
+        "Dot_LGBTQ.png",
+        "Dot_FoodKitchens.png",
+        "Dot_SeniorCenters.png",
+        "Dot_UniPreK.png",
+        "Dot_K12.png",
+        "Dot_CommunityCenters.png",
+        "Dot_MentalHealth.png",
+        "Dot_CBO.png",
+        "Dot_Libraries.png"
+    ];
+    return Promise.all(
+        images.map(
+            img =>
+                new Promise((resolve, reject) => {
+                    map.loadImage(
+                        `${process.env.PUBLIC_URL}/imgs/${img}`,
+                        function(error, res) {
+                            map.addImage(img, res);
+                            resolve();
+                        }
+                    );
+                })
+        )
+    );
 }
 
-export default function useFacilitiesLayer(map, visible, types) {
-  const facilities = useFacilities();
+export default function useFacilitiesLayer(map, visible, selectedTypes) {
+    const facilities = useFacilities();
+    const types = facilities
+        ? facilities.features.reduce((set, f) => {
+              set.add(f.properties.asset_type);
+              return set;
+          }, new Set())
+        : [];
 
-  useEffect(() => {
-    if (map.current && facilities) {
-      map.current.on('load', () => {
-        loadAllImages(map.current).then(() => {
-          map.current.addLayer({
-            id: 'facilities',
-            type: 'symbol',
-            source: {
-              type: 'geojson',
-              data: facilities,
-            },
-            layout: {
-              'icon-image': ['get', 'icon'],
-              'icon-size': 0.02,
-              'text-field': ['get', 'name'],
-              'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
-              'text-offset': [0, 0.6],
-              'text-size': 10,
-              'text-anchor': 'top',
-              //'icon-allow-overlap': true,
-              //              'text-allow-overlap': true,
-              visibility: visible ? 'visible' : 'none',
-            },
-            //    filter: ['match', ['get', 'asset_type'], types, true, false],
+    useEffect(() => {
+        if (map.current && facilities) {
+            map.current.on("load", () => {
+                loadAllImages(map.current).then(() => {
+                    types.forEach(type => {
+                        map.current.addLayer({
+                            id: `facilities: ${type}`,
+                            type: "symbol",
+                            source: {
+                                type: "geojson",
+                                data: {
+                                    ...facilities,
+                                    features: facilities.features.filter(
+                                        f => f.properties.asset_type === type
+                                    )
+                                }
+                            },
+                            layout: {
+                                "icon-image": ["get", "icon"],
+                                "icon-size": 0.02,
+                                "text-field": ["get", "name"],
+                                "text-font": [
+                                    "Open Sans Semibold",
+                                    "Arial Unicode MS Bold"
+                                ],
+                                "text-offset": [0, 0.6],
+                                "text-size": 10,
+                                "text-anchor": "top",
+                                //'icon-allow-overlap': true,
+                                //              'text-allow-overlap': true,
+                                visibility: selectedTypes.includes(type)
+                                    ? "visible"
+                                    : "none"
+                            },
+                            //    filter: ['match', ['get', 'asset_type'], types, true, false],
 
-            paint: {
-              'text-color': 'rgba(255,255,255,1)',
-            },
-          });
+                            paint: {
+                                "text-color": "rgba(255,255,255,1)"
+                            }
+                        });
+                    });
+                });
+            });
+        }
+    }, [map, facilities]);
+
+    useEffect(() => {
+        types.forEach(type => {
+            map.current.setLayoutProperty(
+                `facilities: ${type}`,
+                "visibility",
+                selectedTypes.includes(type) ? "visible" : "none"
+            );
         });
-      });
-    }
-  }, [map, facilities]);
+    }, [selectedTypes, map, types]);
 
-  useEffect(() => {
-    if (map.current && map.current.loaded()) {
-      console.log('setting filter');
-      map.current.setFilter('facilities', [
-        'in',
-        ['get', 'asset-type'],
-        ['literal', types],
-      ]);
-    }
-  }, [types, map]);
+    useEffect(() => {
+        if (map.current && map.current.loaded()) {
+            console.log("Toggling visible on facilities");
+            map.current.setLayoutProperty(
+                `facilities`,
+                "visibility",
+                visible ? "visible" : "none"
+            );
+        }
+    }, [map, visible]);
 
-  useEffect(() => {
-    if (map.current && map.current.loaded()) {
-      console.log('Toggling visible on facilities');
-      map.current.setLayoutProperty(
-        `facilities`,
-        'visibility',
-        visible ? 'visible' : 'none',
-      );
-    }
-  }, [map, visible]);
-
-  return facilities;
+    return facilities;
 }
--- src/hooks/useGeoJSONLayer.js
+++ src/hooks/useGeoJSONLayer.js
@@ -1,73 +1,73 @@
-import {useEffect, useRef} from 'react';
+import { useEffect, useRef } from "react";
 
 export function useGeoJSONLayer(
-  map,
-  name,
-  {url, paintFill, paintLine, sourceLayer, onClick, selection, visible},
+    map,
+    name,
+    { url, paintFill, paintLine, sourceLayer, onClick, selection, visible }
 ) {
-  const fillLayer = useRef(null);
-  const lineLayer = useRef(null);
-  const source = useRef(null);
-  const source_name = `${name}_source`;
-  const oldSelectionID = useRef(null);
-  useEffect(() => {
-    if (map.current) {
-      map.current.on('load', () => {
-        source.current = map.current.addSource(source_name, {
-          type: 'geojson',
-          data: url,
-        });
+    const fillLayer = useRef(null);
+    const lineLayer = useRef(null);
+    const source = useRef(null);
+    const source_name = `${name}_source`;
+    const oldSelectionID = useRef(null);
+    useEffect(() => {
+        if (map.current) {
+            map.current.on("load", () => {
+                source.current = map.current.addSource(source_name, {
+                    type: "geojson",
+                    data: url
+                });
 
-        if (paintFill) {
-          fillLayer.current = map.current.addLayer({
-            id: `${name}-fill`,
-            type: 'fill',
-            source: source_name,
-            paint: paintFill,
-            layout: {
-              visibility: 'visible', // visible ? 'visible' : 'none',
-            },
-          });
+                if (paintFill) {
+                    fillLayer.current = map.current.addLayer({
+                        id: `${name}-fill`,
+                        type: "fill",
+                        source: source_name,
+                        paint: paintFill,
+                        layout: {
+                            visibility: "visible" // visible ? 'visible' : 'none',
+                        }
+                    });
+                }
+                if (paintLine) {
+                    lineLayer.current = map.current.addLayer({
+                        id: `${name}-line`,
+                        type: "line",
+                        source: source_name,
+                        paint: paintLine,
+                        layout: {
+                            visibility: "visible" // visible ? 'visible' : 'none',
+                        }
+                    });
+                }
+                map.current.on("click", `${name}-fill`, e => {
+                    console.log(e.features[0]);
+                    if (onClick) {
+                        onClick(e.features[0]);
+                    }
+                });
+            });
         }
-        if (paintLine) {
-          lineLayer.current = map.current.addLayer({
-            id: `${name}-line`,
-            type: 'line',
-            source: source_name,
-            paint: paintLine,
-            layout: {
-              visibility: 'visible', // visible ? 'visible' : 'none',
-            },
-          });
+    }, [map]);
+
+    useEffect(() => {
+        if (map.current && selection) {
+            if (oldSelectionID.current) {
+                map.current.setFeatureState(
+                    { source: source_name, id: oldSelectionID.current },
+                    { selected: false }
+                );
+            }
+
+            map.current.setFeatureState(
+                { source: source_name, id: selection.id },
+                { selected: true }
+            );
+            oldSelectionID.current = selection.id;
         }
-        map.current.on('click', `${name}-fill`, e => {
-          console.log(e.features[0]);
-          if (onClick) {
-            onClick(e.features[0]);
-          }
-        });
-      });
-    }
-  }, [map]);
+    }, [selection]);
 
-  useEffect(() => {
-    if (map.current && selection) {
-      if (oldSelectionID.current) {
-        map.current.setFeatureState(
-          {source: source_name, id: oldSelectionID.current},
-          {selected: false},
-        );
-      }
-
-      map.current.setFeatureState(
-        {source: source_name, id: selection.id},
-        {selected: true},
-      );
-      oldSelectionID.current = selection.id;
-    }
-  }, [selection]);
-
-  /*  useEffect(() => {
+    /*  useEffect(() => {
     if (map.current && map.current.loaded()) {
       map.current.setLayoutProperty(
         `${name}-fill`,
@@ -82,19 +82,21 @@ export function useGeoJSONLayer(
     }
   }, [map, visible]);
 */
-  useEffect(() => {
-    if (map.current && map.current.loaded()) {
-      console.log(
-        'setting fill color to be ',
-        paintFill['fill-color'],
-        paintFill,
-      );
-      map.current.setPaintProperty(
-        `${name}-fill`,
-        'fill-color',
-        paintFill['fill-color'],
-      );
-    }
-  }, [paintFill]);
-  return {fillLayer, source};
+    useEffect(() => {
+        if (map.current) {
+            const setPaint = () => {
+                if (map.current.loaded()) {
+                    map.current.setPaintProperty(
+                        `${name}-fill`,
+                        "fill-color",
+                        paintFill["fill-color"]
+                    );
+                } else {
+                    setTimeout(setPaint, 1000);
+                }
+            };
+            setPaint();
+        }
+    }, [paintFill]);
+    return { fillLayer, source };
 }
