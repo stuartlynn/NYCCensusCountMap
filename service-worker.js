e87763b02581e912c5a5be7dbf9d81c9f9fb3926
Stuart Lynn <stuart.lynn@gmail.com>
2020-03-25 09:39:22 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a6d5ade99e66ea14a3a15c38e9c676d5.js"
+  "/precache-manifest.1b8b94aca50b0656c09d010420bc14b5.js"
 );
 
 self.addEventListener('message', (event) => {
