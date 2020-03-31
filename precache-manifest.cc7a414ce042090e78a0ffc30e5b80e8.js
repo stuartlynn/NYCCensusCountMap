fde0d1c9654083727e02546e963a99f6a7c2233c
Stuart Lynn <stuart.lynn@gmail.com>
2020-03-31 16:50:31 -0400

    Updates

---
+++ precache-manifest.cc7a414ce042090e78a0ffc30e5b80e8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1025ca15545aa8e4ed625695c3c60cdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03c54b5b04cea0087229",
+    "url": "/static/css/2.f1b4d920.chunk.css"
+  },
+  {
+    "revision": "73a937e0f4265d56700a",
+    "url": "/static/css/main.286f01f1.chunk.css"
+  },
+  {
+    "revision": "03c54b5b04cea0087229",
+    "url": "/static/js/2.f2882ce4.chunk.js"
+  },
+  {
+    "revision": "47b9675648a01ded7e370d9b9a2bda86",
+    "url": "/static/js/2.f2882ce4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "73a937e0f4265d56700a",
+    "url": "/static/js/main.d620a26d.chunk.js"
+  },
+  {
+    "revision": "3a298ab4ed5ea7a1eb17",
+    "url": "/static/js/runtime-main.f01d1c78.js"
+  },
+  {
+    "revision": "1313c346b5eefdd2c3fd440bc9b18002",
+    "url": "/static/media/GT-America-Standard-Bold.1313c346.otf"
+  },
+  {
+    "revision": "ac9e5f9ba79e0a6c4e0c72a295fde8cf",
+    "url": "/static/media/GT-America-Standard-Regular.ac9e5f9b.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.87e478c4afbebef105eac31bf1fb6558.js"
+  "/precache-manifest.cc7a414ce042090e78a0ffc30e5b80e8.js"
 );
 
 self.addEventListener('message', (event) => {
