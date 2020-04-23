c8b6d781d324be146da599070d9d24888d71db98
Stuart Lynn <stuart.lynn@gmail.com>
2020-04-23 18:35:46 -0400

    data for the 23rd

--- src/components/Details.js
+++ src/components/Details.js
@@ -356,7 +356,7 @@ export default function Details({
                                         )}{" "}
                                         %
                                     </p>
-                                    <p>Last updated 21th April</p>
+                                    <p>Last updated 23rd April</p>
                                 </div>
                                 <div className="card comparison">
                                     <p>
@@ -372,11 +372,11 @@ export default function Details({
                                             %{" "}
                                         </span>{" "}
                                         <span style={{ fontWeight: "bold" }}>
-                                            Self response rate as of 21st April
+                                            Self response rate as of 23st April
                                             2020
                                         </span>
                                         <br />
-                                        37th day of counting
+                                        39th day of counting
                                     </p>
                                     <ProgressBar
                                         pc={displayFeature.properties.CRRALL}
@@ -395,11 +395,11 @@ export default function Details({
                                             %{" "}
                                         </span>{" "}
                                         <span style={{ fontWeight: "bold" }}>
-                                            Self response rate as of 20th April
+                                            Self response rate as of 22nd April
                                             2010
                                         </span>
                                         <br />
-                                        37th day of counting
+                                        39th day of counting
                                     </p>
                                     <ProgressBar
                                         pc={
