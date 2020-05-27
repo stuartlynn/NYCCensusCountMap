64d75b03d9b637653c5e21e487e1027ca16d6836
Stuart Lynn <stuart.lynn@gmail.com>
2020-05-27 10:05:10 -0400

    Updates

---
+++ precache-manifest.b644ff1c5086cc59ae37161116901ada.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d0d7df8c237ad27a9b5953a0ac9c9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa931374065102496c22",
+    "url": "/static/css/2.76609786.chunk.css"
+  },
+  {
+    "revision": "1e5abbff88f54b50e734",
+    "url": "/static/css/main.f7a6e938.chunk.css"
+  },
+  {
+    "revision": "fa931374065102496c22",
+    "url": "/static/js/2.c232d224.chunk.js"
+  },
+  {
+    "revision": "e928fe3a96a63c8d640bfb97f49535de",
+    "url": "/static/js/2.c232d224.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e5abbff88f54b50e734",
+    "url": "/static/js/main.ff610b17.chunk.js"
+  },
+  {
+    "revision": "78a6063247de8deafb56",
+    "url": "/static/js/runtime-main.278af6e7.js"
+  },
+  {
+    "revision": "1313c346b5eefdd2c3fd440bc9b18002",
+    "url": "/static/media/GT-America-Standard-Bold.1313c346.otf"
+  },
+  {
+    "revision": "ac9e5f9ba79e0a6c4e0c72a295fde8cf",
+    "url": "/static/media/GT-America-Standard-Regular.ac9e5f9b.otf"
+  },
+  {
+    "revision": "557633ad25205c6875c6a68e9d7ff0e6",
+    "url": "/static/media/laundry_484949.557633ad.svg"
+  },
+  {
+    "revision": "59ec3d76819bc4da23eb60a9c3547fd8",
+    "url": "/static/media/laundry_A44098.59ec3d76.svg"
+  },
+  {
+    "revision": "9d18bb14727e1151f0de7ad17bf7c33d",
+    "url": "/static/media/laundry_A44098_alert.9d18bb14.svg"
+  }
+]);
