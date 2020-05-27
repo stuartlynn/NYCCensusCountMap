84fc4c88d4b73fce2020784e7ac9cc6fc2d5e10b
Stuart Lynn <stuart.lynn@gmail.com>
2020-05-27 09:24:18 -0400

    removing stray $

--- src/components/Details.js
+++ src/components/Details.js
@@ -365,7 +365,7 @@ export default function Details({
                                         )}{" "}
                                         %
                                     </p>
-                                    <p>Last updated ${todayString}</p>
+                                    <p>Last updated {todayString}</p>
                                 </div>
                                 <div className="card comparison">
                                     <p>
