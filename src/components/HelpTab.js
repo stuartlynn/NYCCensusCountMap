99284c5f7d9619243f4688541e6b30d6fc2b5ed8
Stuart Lynn <stuart.lynn@gmail.com>
2020-10-21 16:08:14 -0400

    new about text

--- src/components/HelpTab.js
+++ src/components/HelpTab.js
@@ -5,38 +5,12 @@ export default function HelpTab() {
         <div className="help-tab">
             <section>
                 <h3>ABOUT</h3>
-
-                <p>
-                    This interactive map was created by{" "}
-                    <a href="https://hesterstreet.org/" target="_blank">
-                        Hester Street
-                    </a>{" "}
-                    in partnership with{" "}
-                    <a href="http://stuartlynn.me/">Stuart Lynn</a>. Hester
-                    Street is an urban planning, design and development
-                    nonprofit that works to ensure neighborhoods are shaped by
-                    the people who live in them.
-                </p>
-                <p>
-                    This work was made possible with the support of the NYC
-                    Census 2020 and CUNY Complete Count Fund.
-                </p>
-                <p>
-                    If you have additional assets, services or neighborhood
-                    institutions to contribute to the map, please email{" "}
-                    <a href="2020Census@hesterstreet.org">
-                        2020Census@hesterstreet.org
-                    </a>{" "}
-                    with the subject line "New interactive map assets". Be sure
-                    to include the following information for each asset
-                </p>
-                <ul className="dashed">
-                    <li>Name </li>
-                    <li>Address</li>
-                    <li>Layer it should be added to</li>
-                </ul>
-                <img src="imgs/HS_Logo_Blk_RGB.png" />
-            </section>
+<p>
+This platform was created by <a target="_blank" href='https://hesterstreet.org/'>Hester Street</a> in partnership with <a href='http://stuartlynn.me' target="_blank">Stuart Lynn</a>. Hester Street is an urban planning, design and development nonprofit that works to ensure neighborhoods are shaped by the people who live in them.</p>
+<p>
+If you would like to learn more about this map or discuss its features please email <a href="mailto:vote@hesterstreet.org">vote@hesterstreet.org</a>. 
+</p>
+</section>
         </div>
     );
 }
